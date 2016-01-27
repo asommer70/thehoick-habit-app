8cbaa6c0f9430dfe2994a57df575ef36b752f731
Adam Sommer <asommer70@gmail.com>
2016-01-27 09:42:42 -0500

    Have confirmation popup for all destructive methods.

--- src/habits.js
+++ src/habits.js
@@ -126,7 +126,6 @@ module.exports = React.createClass({
   },
 
   addiOSReminder: function() {
-    // var date = this.state.chosenDate;
     var habits = this.props.habits;
     var momentDate = moment(this.state.chosenDate);
 
@@ -175,24 +174,13 @@ module.exports = React.createClass({
   },
 
   addAndroidReminder: function(habitIdx) {
-    // this.popup.alert(1, 'beans...');
-    // this.popup.tip({
-    //     title: 'Choose Reminder Time',
-    //     content: <AndroidDate events={this.props.events} />,
-    // });
-    //return <AndroidDate events={this.props.events} />
-
     var habits = this.props.habits;
 
     if (habits[habitIdx].reminder === undefined || habits[habitIdx].reminder === null) {
       // Create new Calendar event.
       NativeModules.DateAndroid.showTimepicker(function() {}, (hour, minute) => {
-        // this.props.events.emit('date-changed', hour + ":" + minute);
-
         // Reound the minute to the nearest 10 to make things look cleaner on the Calendar.
         minute = Math.round(minute / 10) * 10;
-
-        // var momentDate = moment(this.state.chosenDate);
 
         habits[habitIdx].reminder = hour + ":" + minute;
         store.save('habits', this.props.habits);
@@ -220,6 +208,22 @@ module.exports = React.createClass({
       // Open Calendar for editing reminder event.
       SendIntentAndroid.sendOpenCalendar();
     }
+  },
+
+  confirm: function(content, callbackFunc, callbackArgs) {
+    // Close the Modal if it's open.
+    if (this.state.modalVisible) {
+      this.setState({modalVisible: false});
+    }
+
+    this.popup.confirm({
+        content: content,
+        ok: {
+            callback: () => {
+                this[callbackFunc](callbackArgs);
+            },
+        },
+    });
   },
 
   removeReminder: function(visible) {
@@ -260,11 +264,6 @@ module.exports = React.createClass({
     }
   },
 
-  // onPressHandle: function() {
-  //     // alert
-  //     this.popup.alert(1, 'two');
-  // },
-
   habitComponents: function() {
     var habits = this.props.habits.map((habit, index) => {
       return (
@@ -276,15 +275,17 @@ module.exports = React.createClass({
 
             <LinkCount habit={habit} linkCountStyle={styles.linkCountText} events={this.props.events}/>
 
-            <TouchableHighlight underlayColor={'gray'} style={styles.habitButton} onPress={() => this.removeReminder(index)}>
+            <TouchableHighlight underlayColor={'gray'} style={styles.habitButton} onPress={
+                () => this.confirm('Really remove remove reminder?', 'removeReminder', index)
+            }>
               <Text style={styles.reminderText}>Reminder: {habit.reminder ? habit.reminder : 'No Reminder'}</Text>
             </TouchableHighlight>
           </View>
 
           <View style={styles.habitButtons}>
             <Button imageStyle={styles.iconImage} imageSrc={require('./img/alarm-clock-icon.png')} onPress={() => this.openModal(index)} buttonType={styles.restartButton} />
-            <Button imageStyle={styles.iconImage} imageSrc={require('./img/reload-icon.png')} onPress={() => this.restartHabit(index)} buttonType={styles.restartButton} />
-            <Button imageStyle={styles.iconImage} imageSrc={require('./img/trash-icon.png')} onPress={() => this.deleteHabit(index)} buttonType={styles.deleteButton} />
+            <Button imageStyle={styles.iconImage} imageSrc={require('./img/reload-icon.png')} onPress={() => this.confirm('Really restart chain?', 'restartHabit', index)} buttonType={styles.restartButton} />
+            <Button imageStyle={styles.iconImage} imageSrc={require('./img/trash-icon.png')} onPress={() => this.confirm('Really delete habit?', 'deleteHabit', index)} buttonType={styles.deleteButton} />
           </View>
         </View>
       )
@@ -325,7 +326,7 @@ module.exports = React.createClass({
             <View style={[styles.innerContainer]}>
               {React.Platform.OS == 'ios' ? <IOSDate events={this.props.events} /> : <View/>}
               <Button text={'Set Time'} onPress={this.closeModal.bind(this, false)} textType={styles.restartText} buttonType={styles.restartButton} />
-              <Button text={'Remove Reminder'} onPress={this.removeReminder.bind(this, false)} textType={styles.deleteText} buttonType={styles.deleteButton} />
+              <Button text={'Remove Reminder'} onPress={this.confirm.bind(this, 'Really remove remove reminder?', 'removeReminder', false)} textType={styles.deleteText} buttonType={styles.deleteButton} />
             </View>
           </View>
         </Modal>
