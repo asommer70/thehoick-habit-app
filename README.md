0097fdc0265701ec7acf553cb00ccdacf15a3eae
Adam Sommer <asommer70@gmail.com>
2016-02-03 08:51:29 -0500

    Created gh-pages branch, removed React Native app files, and setup Foundation template.

--- README.md
+++
@@ -1,2 +0,0 @@
-# The Hoick Habit App
-A habit building app based on the ["Don't break the chain"](http://lifehacker.com/281626/jerry-seinfelds-productivity-secret) philosophy.
--- index.android.js
+++
@@ -1,4 +0,0 @@
-var React = require('react-native');
-var { AppRegistry } = React;
-var Nav = require('./src/nav');
-AppRegistry.registerComponent('thehoick.habit_app', () => Nav);
